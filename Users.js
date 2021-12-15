c11a91fd80d43607b03beacca1910ffce85e6b0d
chitravelayutham <chitra_velayutham@yahoo.ca>
2021-12-14 19:39:10 -0500

    Initial commit

---
+++ Users.js
@@ -0,0 +1,24 @@
+//Users.js is a model that contains data on all users
+const users = [{
+      id: 1,
+      name: "John",
+	  email: "john@gmail.com"
+      }, 
+	  {  
+      id: 2,
+      name: "Smith",
+      email: "smith@gmail.com"
+	  }, 
+	  {
+      id: 3,
+      name: "Chris",
+      email: "chris@gmail.com"
+      }, 
+	  {
+      id: 4,
+      name: "Jack",
+      email: "jack@gmail.com"
+      }];
+  
+     
+  module.exports = users;
---
+++ index.js
@@ -0,0 +1,26 @@
+//The file index.js acts as the main server for the Node.js application
+
+//This is the file that acts as the main server and binds every API route together. 
+//We create this file in the root directory of the project and then add the following code:
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/users", require("./routes/api/users")); 
+app.listen(3000, () => console.log('Server started'));
+
+ 
+/*
+To run: 
+node index.js 
+Server started 
+
+Use Postman for API calls:
+GET to retrieve data: http://localhost:3000/api/users/
+POST with JSON body to add a user: http://localhost:3000/api/users/
+PUT with JSON body to update data for user-id: http://localhost:3000/api/users/1
+DELETE to delete a user: http://localhost:3000/api/users/1
+
+*/
